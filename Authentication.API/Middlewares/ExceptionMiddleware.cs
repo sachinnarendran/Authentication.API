7910da28a08c33f14a53c71ba8d0732150511a41
Sachin Narendran <sachinnarendran@gmail.com>
2021-10-06 10:01:59 +0530

    Exception Middleware Changes

---
+++ Authentication.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Authentication.API.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        public ExceptionMiddleware(RequestDelegate requestDelegate)
+        {
+            requestDelegate = _requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch(Exception ex)
+            {                
+                
+                Console.WriteLine(ex.Message.ToString());
+                await HandleExceptionAsync(context, ex);
+                
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context,Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsync(new ApiError()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error Occured"
+            }.ToString());
+
+        }
+    
+    }
+}
